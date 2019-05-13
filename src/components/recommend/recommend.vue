72d6ead2b5d3540a60968aa9c0a7a96404069be1
liuzhe <1040583465@qq.com>
2019-05-13 13:31:37 +0800

    swiper修复

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,46 +1,36 @@
 <template>
   <div>
-    <el-carousel
-      trigger="click"
-      height="30vh"
-      indicator-position="outside"
-      arrow="never"
-      :interval="4000"
-      v-loading="loading"
-    >
-      <el-carousel-item v-for="item in recommendData.slider" :key="item.id">
-        <a :href="item.linkUrl">
-          <img :src="item.picUrl" alt>
-        </a>
-      </el-carousel-item>
-    </el-carousel>
+    <div class="swiper" v-loading="loading">
+      <swiper ref="mySwiper" v-if="recommendData.slider.length>0">
+        <swiper-slide v-for="item in recommendData.slider" :key="item.id">
+          <a :href="item.linkUrl">
+            <img :src="item.picUrl" alt>
+          </a>
+        </swiper-slide>
+      </swiper>
+    </div>
     <div>热门歌单推荐</div>
   </div>
 </template>
 <script>
 import { getRecommend } from "../../api/recommend.js";
 import { ERR_OK } from "../../api/config.js";
-import {getDiscList} from "../../api/recommend.js"
+import { getDiscList } from "../../api/recommend.js";
 export default {
   created() {
     console.log(1);
+    this.loading = true;
     this._getRecommend();
     // this._getDiscList()
   },
   methods: {
     _getRecommend() {
-      this.loading = true;
       getRecommend().then(res => {
         this.recommendData = res.data;
         console.log(this.recommendData);
         this.loading = false;
       });
-    },
-    // _getDiscList(){
-    //     getDiscList().then(res=>{
-    //         console.log(res)
-    //     })
-    // }
+    }
   },
   data() {
     return {
@@ -55,11 +45,11 @@ export default {
 };
 </script>
 <style>
-.el-carousel__item img {
+.swiper-slide img {
   width: 100%;
   height: 100%;
 }
-button{
-    z-index:999
+.swiper{
+  height: 23vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import VueAwesomeSwiper from 'vue-awesome-swiper' //swiper移动端插件
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper,{loop: true,speed: 900,autoplay: {delay: 3000,disableOnInteraction: false},} )
 //使用jsonp的库:jsonp(url,optional,callback)
 Vue.config.productionTip = false
 Vue.use(ElementUI)

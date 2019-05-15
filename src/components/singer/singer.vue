75179e48b7234bee538891c2d201c706b8af35a0
actt0422 <actt422@outlook.com>
2019-05-16 02:16:43 +0800

    singerList

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <div class = "title">
+        <!-- <div class = "title">
             singerList
-        </div>
+        </div> -->
         <div class="singerList">
             <ul>
-                <li v-for="item in singerData.singerList.data" :key="item.id">
-
-
+                <li v-for="item in singerData.singerList" :key="item.id">
+                    <div class="singerListItem">
+                        <div class="singerListPic">
+                            <img :src="item.singer_pic" alt="">
+                        </div>
+                        <div class="listSingerName">
+                            <p>{{item.singer_name}}</p>
+                        </div>
+                        <i class="el-icon-arrow-right"></i>
+                    </div>
                 </li>
 
             </ul>
@@ -16,7 +23,7 @@
 </template>
 
 <script>
-import { getRecommend } from "../../api/recommend.js";
+// import { getRecommend } from "../../api/recommend.js";
 import { ERR_OK } from "../../api/config.js";
 export default{
     created() {
@@ -48,3 +55,54 @@ export default{
     }
 };
 </script>
+
+<style>
+
+.singerListItem{
+    border-bottom: 0.5px #00000066 solid;
+}
+.singerList ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.singerList ul li{
+    height: 50px;
+}
+.singerListPic{
+    display: inline-block;
+    float: left;
+    height: 50px;
+    box-sizing: border-box;
+    padding-top: 5px;
+    padding-left: 10px;
+}
+.singerListPic img{
+    width:40px;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+}
+.listSingerName{
+    display: inline-block;
+    float: left;
+}
+.listSingerName p{
+    /* display: inline-block; */
+    font-size: 14px;
+    line-height: 47px;
+    padding: 0;
+    margin: 0;
+    padding-left: 20px;
+}
+
+.el-icon-arrow-right{
+    display: inline-block;
+    float: right;
+    line-height: 47px;
+    padding-right: 10px;
+    color:#919191;
+}
+</style>

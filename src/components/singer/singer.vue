96f4b7028e8e819e5196052cacfa77b32656d18b
actt0422 <actt422@outlook.com>
2019-05-15 15:59:19 +0800

    我也不懂

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,5 +1,49 @@
 <template>
     <div>
-        singer
+        <div class = "title">
+            singerList
+        </div>
+        <div class="singerList">
+            <ul>
+                <li v-for="item in singerData.singerList.data" :key="item.id">
+
+
+                </li>
+
+            </ul>
+        </div>
     </div>
 </template>
+
+<script>
+import { getRecommend } from "../../api/recommend.js";
+import { ERR_OK } from "../../api/config.js";
+export default{
+    created() {
+        this.getSingerList();
+        this.listLoading = false;
+    },
+    methods:{
+        getSingerList(){
+          this.listLoading = true;
+          this.$api
+            .get(
+              "tencent/artist/list?page=0&pageSize=30"
+            )
+            .then(res => {
+                this.singerData.singerList = res.data;
+                this.listLoading = false;
+            });
+        }
+    },
+    data(){
+        return {
+            singerData:{
+                slider:[],
+                singerList:[]
+            },
+            loading:false
+        };
+    }
+};
+</script>

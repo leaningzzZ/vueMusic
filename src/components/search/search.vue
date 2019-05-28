c47decfa01aa188b84ad6cad9b2423a3ed9ed82d
actt0422 <actt422@outlook.com>
2019-05-28 15:01:22 +0800

    歌手，搜索

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <input type="text" v-model="keyword">
+        <el-input class="search" placeholder="搜索歌曲丶歌单丶专辑" prefix-icon="el-icon-search"  v-model="keyword" v-on:keyup.enter="search(keyword)" > </el-input>
         <button @click="search(keyword)">搜索</button>
     </div>
 
@@ -22,3 +22,13 @@ export default {
     }
 }
 </script>
+
+<style>
+
+input{
+    /* width: 100%; */
+    font-size: 15px;
+}
+
+
+</style>

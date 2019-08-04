30d1d3929282779a56003dabc4a4198441e1e6e9
actt0422 <actt422@outlook.com>
2019-08-04 19:01:05 +0800

    删除rank

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -6,9 +6,6 @@
         <router-link  tag="div" class="tab-item" active-class="active" to="/singer">
             <span class="tab-link">歌手</span>
         </router-link>
-        <router-link  tag="div" class="tab-item" active-class="active" to="/rank">
-            <span class="tab-link">排行</span>
-        </router-link>
         <router-link  tag="div" class="tab-item" active-class="active" to="/search">
             <span class="tab-link">搜索 </span>
         </router-link>
